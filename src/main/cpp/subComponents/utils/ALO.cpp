a8f00c78eed243a475acbbe0fcb94f5cdb93d22a
Aidan747 <aidan747-8@outlook.com>
2022-05-06 09:02:49 -0500

    pre reverse kinematics

--- src/main/cpp/subComponents/utils/ALO.cpp
+++ src/main/cpp/subComponents/utils/ALO.cpp
@@ -4,7 +4,8 @@
 
 #include "subComponents/utils/ALO.h"
 
-#define REGRESSION(x) 2276.083 - (-84.4069/-0.05685471)*(1 - std::exp((0.05685471*x)))
+#define SHOOTER_REGRESSION(x) 2276.083 - (-84.4069/-0.05685471)*(1 - std::exp((0.05685471*x)))
+#define SPEED_CURVE(x) 0.6914895 + (0.08625241 - 0.6914895) / (1 + pow((inputSpeed / 0.3315671), 11.36875))
 
 ALO::ALO() = default;
 
@@ -14,10 +15,10 @@ double ALO::getRPMFromDistance(double distance) {
             return distanceMapItr->second;
         }
     }
-    double calcualtedRPM = REGRESSION(distance);
-    return calcualtedRPM; 
+    double calcualtedRPM = SHOOTER_REGRESSION(distance);
+    return calcualtedRPM;
 }
 
 double ALO::curveSpeed(double inputSpeed) {
-    return 0.6914895 + (0.08625241 - 0.6914895) / (1 + pow((inputSpeed / 0.3315671), 11.36875));
+    return SPEED_CURVE(inputSpeed);
 }
